9832d0dda9a1c5d9b1f8d77b6f1c690998eb4884
zharifa <zharifanurmajidah@students.untidar.ac.id>
2024-08-27 12:41:54 +0700

    Zharifa_Prak2PBO

---
+++ 2320506021_ZharifaNurMajidah_Prak2PBO.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.zharifa_project1;
+
+/**
+ *
+ * @author FC COM
+ */
+public class Zharifa_project1 {
+    public static void main(String[] args) {
+        // Menyimpan informasi pribadi
+        String nama = "Zharifa Nur Majidah";
+        String npm = "2320506021";
+        String alamat = "Pemalang";
+        String noHp = "087821515028";
+
+        // Menampilkan informasi pribadi
+        System.out.println("Nama  : " + nama);
+        System.out.println("NPM   : " + npm);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("No HP : " + noHp);
+    }
+}
